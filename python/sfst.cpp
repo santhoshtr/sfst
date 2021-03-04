cb2cbb34f7ea04d0d8ab8c966a98cfb2138e49e5
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-03-04 20:49:19 +0530

    Simplify and format binding code

--- python/sfst.cpp
+++ python/sfst.cpp
@@ -6,10 +6,9 @@
 using namespace SFST;
 
 const int GENERATE_MODE = 1;
-const int ANALYSE_MODE  = 2;
+const int ANALYSE_MODE = 2;
 
 Transducer *transducer;
-char *temp_filename = "None";
 
 /*
  * Use the transducer (either in generate- or in analyse-mode) to rewrite the
@@ -19,61 +18,76 @@ char *temp_filename = "None";
  * to access the output of the analyse_string/generate_string directly. As a
  * workaround the result is written into a file and afterwards read out.
  */
-const char *transduce(char input[], int mode){
-	FILE *temp_file;
+const char *transduce(char input[], int mode)
+{
+    FILE *temp_file = std::tmpfile();
 
- 	// Analyse or generate transducer output and write to file
- 	temp_file = fopen(temp_filename, "w");
-	if (mode == ANALYSE_MODE) {
- 		transducer->analyze_string(input, temp_file, true);
-	} else if (mode == GENERATE_MODE) {
-		transducer->generate_string(input, temp_file, true);
-	}
- 	fclose(temp_file);
+    // Analyse or generate transducer output and write to file
+    if (mode == ANALYSE_MODE)
+    {
+        transducer->analyze_string(input, temp_file, true);
+    }
+    else if (mode == GENERATE_MODE)
+    {
+        transducer->generate_string(input, temp_file, true);
+    }
+    std::rewind(temp_file);
+    // Read transducer output from file
+    char c;
+    std::ostringstream oss(std::ostringstream::out);
+    c = fgetc(temp_file);
+    while (c != EOF)
+    {
+        oss.put(c);
+        c = fgetc(temp_file);
+    }
+    fclose(temp_file);
 
- 	// Read transducer output from file
- 	char c;
- 	std::ostringstream oss(std::ostringstream::out);
- 	temp_file = fopen(temp_filename, "r");
- 	c = fgetc(temp_file);
- 	while (c != EOF) {
- 		oss.put(c);
- 		c = fgetc(temp_file);
- 	}
- 	fclose(temp_file);
-
-	return oss.str().c_str();
+    return oss.str().c_str();
 }
 
-void init(char *transducer_filename){
-     // Initialize transducer.
-	FILE *transducer_file;
+/**
+ * Initialize transducer.
+ */
+void init(char *transducer_filename)
+{
     if (transducer_filename == NULL)
     {
         printf("Please provide transducer file");
         exit(1);
     }
-	transducer_file = fopen(transducer_filename, "rb");
-	transducer = new Transducer(transducer_file);
-	fclose(transducer_file);
+    FILE *transducer_file;
+    transducer_file = fopen(transducer_filename, "rb");
+    if (transducer_file != NULL)
+    {
+        transducer = new Transducer(transducer_file);
+        fclose(transducer_file);
+    }
+    else
+    {
+        perror("Could not read transducer file");
+        exit(1);
+    }
 }
 
 /*
  * Destroys the transducer object.
  * args: None
  */
-void delete_transducer(){
+void delete_transducer()
+{
     delete transducer;
 }
 
-const char *analyse(char *analysis_input){
+const char *analyse(char *analysis_input)
+{
     return transduce(analysis_input, ANALYSE_MODE);
 }
 
-const char *generate(char *generate_input){
+const char *generate(char *generate_input)
+{
     return transduce(generate_input, GENERATE_MODE);
 }
-
 
 namespace py = pybind11;
 
