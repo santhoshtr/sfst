1f510678fde9f17ac66b4e0a55ccda749ace9408
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-03-07 16:10:45 +0530

    Fix the current path calculation

--- python/tests/test.py
+++ python/tests/test.py
@@ -3,7 +3,9 @@ import sfst
 import pathlib
 import os
 
+CURR_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
 print("SFST version " + sfst.__version__)
-sfst.init( os.path.join(pathlib.Path(__file__).parent, "easy.a"))
+sfst.init(os.path.join(CURR_DIR, 'easy.a'))
 assert sfst.analyse("easy") == 'easy<ADJ><pos>\n'
 assert sfst.generate("easy<ADJ><pos>") == 'easy\n'
