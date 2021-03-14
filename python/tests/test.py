ac862803856b4e1c4026ce5c4c5af66acee75eb8
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-03-14 10:39:46 +0530

    Debug the result of analysis

--- python/tests/test.py
+++ python/tests/test.py
@@ -6,5 +6,6 @@ CURR_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
 print("SFST version " + sfst.__version__)
 sfst.init(os.path.join(CURR_DIR, 'easy.a'))
+print("Analyse 'easy': " + sfst.analyse("easy") )
 assert sfst.analyse("easy") == 'easy<ADJ><pos>'
 assert sfst.generate("easy<ADJ><pos>") == 'easy'
