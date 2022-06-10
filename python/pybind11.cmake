1da71cca153b060aa80ae18b65128281c8a9f2af
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2022-06-10 10:31:03 +0530

    Use latest pybind11 2.9.2 version (#20)

--- python/pybind11.cmake
+++ python/pybind11.cmake
@@ -4,7 +4,7 @@ set(FETCHCONTENT_QUIET FALSE)
 
 FetchContent_Declare(
     pybind11
-    URL https://github.com/pybind/pybind11/archive/v2.6.2.tar.gz
+    URL https://github.com/pybind/pybind11/archive/v2.9.2.tar.gz
     )
 
 FetchContent_GetProperties(pybind11)
